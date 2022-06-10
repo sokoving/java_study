89fe1e8e45d48e4b2ad297f2a022d131d75a9d43
sokoving <sokoving@gmail.com>
2022-06-10 18:16:48 +0900

    0610 1816

--- src/day11/inherit/Player.java
+++ src/day11/inherit/Player.java
@@ -4,12 +4,15 @@ package day11.inherit;
 // extends Object는 생략돼 있지만 자동 삽입돼 있다
 public class Player extends Object{
     public String name; // 캐릭터 이름
+    public String job; // 캐릭터 직업
     public int level; // 레벨
     public int hp; // 체력
     public int exp; // 경험치
 
-    public Player(String name){
-        System.out.println("플레이어 생성자 호출");
+    public Player(String name)
+    {
+        this.name = name;
+        this.hp = 50;
     }
 
     public void showStatus(){
@@ -17,5 +20,13 @@ public class Player extends Object{
         System.out.println(" # name: " + name);
         System.out.println(" # level: " + level);
         System.out.println(" # hp: " + hp);
+    }
+
+    public static void showHp(Player... p){
+        System.out.println("========================");
+        for (Player player : p) {
+            System.out.printf("%s(%s) HP : %d\n", player.name, player.job, player.hp);
+        }
+        System.out.println("========================");
     }
 }
